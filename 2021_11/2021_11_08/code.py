29a70cac9f18dc255f2d7ffd4e205c989c001ccb
개발하는 곰탱이 <50730897+DeveloperHunny@users.noreply.github.com>
2021-11-08 14:23:00 +0000

    2021_11_08

---
+++ 2021_11/2021_11_08/code.py
@@ -0,0 +1,83 @@
+#baekjoon_1025_제곱수 찾기
+import math
+
+#=== 함수 ===#
+def isSquareNum(num):
+  if math.sqrt(num).is_integer(): 
+    return True;
+  else return False;
+
+#=== 선언 ===#
+row = 0; col = 0;
+startPoint = [];
+secondPoint = [];
+otherPoint =[];
+selectedNum = [];
+diff = [];
+table = [];
+max_squareNum = 0;
+
+
+#=== 입력 ===#
+row, col = map(int,input().split());
+for _ in range(row):
+  tmpList = list(map(int,list(input())));
+  table.append(tmpList);
+
+#=== 풀이 ===#
+
+#startPoint 선택
+for i in range(row):
+  for j in range(col):
+    #초기화
+    selectedNum = []; #selectedNum 초기화;
+    
+    
+    startPoint = [i,j];
+    selectedNum.append(table[i][j]); #selectedNum배열에 추가
+
+    #secondPoint 선택
+    for p in range(row):
+      for q in range(col):
+        diff = []; #diff 초기화
+
+        if p == i and q == j: continue; #같은 칸은 피해야지.
+        secondPoint = [p,q];
+        selectedNum.append(table[p][q]); #selectedNum배열에 추가
+        diff = [p-i, q-j]; #등차 결정.
+
+        otherPoint = secondPoint;
+        #otherPoint 선택
+        while(1):
+          y = otherPoint[0] + diff[0];
+          x = otherPoint[1] + diff[1];
+          if row <= y or col <= x: break; #범위 넘어가면 루프 탈출
+
+          otherPoint = [y,x];
+          selectedNum.append(table[y][x]); #selectedNum배열에 추가
+
+        num = int(selectedNum);
+        if isSquareNum(num):
+          if num > max_squareNum:
+            max_squareNum = num;
+
+
+print(max_squareNum);
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
