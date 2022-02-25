c0060744770a61b19b56e17ebf408796eca5c863
개발하는 곰탱이 <50730897+DeveloperHunny@users.noreply.github.com>
2022-02-25 12:18:59 +0000

    2022_02_25

--- main.py
+++ main.py
@@ -1,44 +1,51 @@
-#baekjoon_1029_그림교환
+#programmers_완전탐색_모의고사
 
 #=== import module ===#
-from collections import deque
+
 #=== variable declare ===#
-dp = [];
+person_solve = [[],[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]];
+
 #=== Function define ===#
-def DFS(curPrice, curPerson, curVisited):
-  global N
+def solve(answers,person):
+  collect = 0;
+  iter = 0;
+  for answer in answers:
+    if person_solve[person][iter] == answer:
+      collect += 1;
+    iter += 1;
 
-  if dp[curPerson][curPrice][curVisited] == None: #예전에 계산해놓은 값 존재 X
-    max_result = 0;
-    for nextPerson in range(N):
-      if curVisited & (1 << nextPerson) != 0: continue; #이미 방문한 적 있음.
-      if curPrice > info[curPerson][nextPerson] : continue; #조건 만족 X
-      max_result = max(max_result, DFS(info[curPerson][nextPerson],
-        nextPerson, curVisited | (1 << nextPerson)) +1 );
-    dp[curPerson][curPrice][curVisited] = max_result;      
+    if iter == len(person_solve[person]): #끝 지점 도달-> 첫 지점으로 돌아감
+      iter = 0;
+
+  return collect;
+    
+
+    
+    
+
+
+def solution(answers):
+  collects = [];
+  collects.append(solve(answers,1));
+  collects.append(solve(answers,2));
+  collects.append(solve(answers,3));
+
+  max_collects = -1;
+  answer = [];
+  for i in range(3):
+    if max_collects < collects[i]:
+      max_collects = collects[i];
+      answer = [];
+    elif max_collects == collects[i]:
+      answer.append(i);
+    else:
+      pass;
+
+  return answer;
+
   
-  if dp[curPerson][curPrice][curVisited] == None: #더 이상 진행X
-    dp[curPerson][curPrice][curVisited] = 0;
-
-
-  return dp[curPerson][curPrice][curVisited];
-
-
-  
-  
-  
-
-      
-
-
-
 #=== main function ===#
-N = int(input()) #아티스트 수
-info = [list(map(int,list(input()))) for _ in range(N)];
-dp = [[[None for _ in range(1 << N)] for _ in range(10)] for _ in range(N)];
 
-print(DFS(0,0,1) + 1);
-
-
+print(solution([1,2,3,4,5]));
 
 
