f6030ddead4ba50320b1f42966841d0f0dbcdc51
개발하는 곰탱이 <50730897+DeveloperHunny@users.noreply.github.com>
2022-02-26 06:26:45 +0000

    2022_02_26

---
+++ 2022_02/26/programmers_모의고사.py
@@ -0,0 +1,52 @@
+#programmers_완전탐색_모의고사
+
+#=== import module ===#
+
+#=== variable declare ===#
+person_solve = [[],[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]];
+
+#=== Function define ===#
+def solve(answers,person):
+  collect = 0;
+  iter = 0;
+  for answer in answers:
+    if person_solve[person][iter] == answer:
+      collect += 1;
+    iter += 1;
+
+    if iter == len(person_solve[person]): #끝 지점 도달-> 첫 지점으로 돌아감
+      iter = 0;
+
+  return collect;
+    
+
+    
+    
+
+
+def solution(answers):
+  collects = [0];
+  collects.append(solve(answers,1));
+  collects.append(solve(answers,2));
+  collects.append(solve(answers,3));
+
+  max_collects = -1;
+  answer = [];
+  for i in range(1,4):
+    if max_collects < collects[i]:
+      max_collects = collects[i];
+      answer = [];
+      answer.append(i);
+    elif max_collects == collects[i]:
+      answer.append(i);
+    else:
+      pass;
+
+  return answer;
+
+  
+#=== main function ===#
+
+print(solution([1,3,2,4,2]));
+
+
