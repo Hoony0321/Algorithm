0e28ff3a66747a8d6a69dc1da4857d48b4d35b25
DeveloperHunny <eogns0321@gmail.com>
2022-05-05 22:37:21 +0900

    2022-05-05

--- 2022_05/04/baekjoon_1238.py
+++ 2022_05/04/baekjoon_1238.py
@@ -11,12 +11,24 @@ def dijkstra(start):
     V = [ i for i in range(1,N+1)];
     Y = [];
 
-    length = [ None for i in range(N+1)]; #start부터 i까지 거리
-    vnear = -1;
+    length = [None for _ in range(N+1)]; #start부터 i까지 거리
+    touch = [start for _ in range(N+1)]; #start가 제일 가까운 정점으로 다 세팅
+
+    edges = set();
+
+
     for i in range(1, N+1): #1부터 N까지
         length[i] = W[start][i];
 
     for _ in range(N): #N-1회 반복
+        min = float('INF');
+        vnear = -1;
+        for vertex in range(1,N+1): #1부터 N까지 반복
+            if(min > length[vertex]):
+                min = length[vertex];
+                vnear = vertex;
+
+        edges.add([touch[vnear], vnear]);
 
 
 
