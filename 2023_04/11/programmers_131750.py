3e1b117b1b3343fde29a8ac13b7b0ecf278441e9
hoony0321 <eogns0321@gmail.com>
2023-04-12 00:44:03 +0900

    2023/04/11

---
+++ 2023_04/11/programmers_131750.py
@@ -0,0 +1,13 @@
+# 문제 정보
+# 삼총사
+
+from itertools import combinations
+def solution(number):
+    answer = 0
+    
+    for comb in combinations(number, 3):
+        if sum(comb) == 0 : answer += 1
+    
+    return answer
+
+
