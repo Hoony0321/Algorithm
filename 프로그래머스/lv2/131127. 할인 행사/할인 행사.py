578b465e0885aedf4a41e7f3219d1d39330ea04e
Daehoon Go <50730897+Hoony0321@users.noreply.github.com>
2023-08-26 22:45:49 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -1,32 +1,13 @@
+from collections import Counter
 def solution(want, number, discount):
     answer = 0
-    dict_discount = dict()
+    dict_want = dict()
     
-    for item in set(discount):
-        dict_discount[item] = 0
+    for i in range(len(want)):
+        dict_want[want[i]] = number[i]
     
-    for item in discount[0:10]:
-        dict_discount[item] += 1
-
-    
-    day = 0
-    while(day <= len(discount)-10):
-        isFind = True
-        for i in range(len(want)):
-            try :
-                if dict_discount[want[i]] != number[i]:
-                    isFind = False
-                    break
-            except KeyError:
-                return 0
-        
-        if(isFind):
+    for i in range(len(discount)-9):
+        if dict_want == Counter(discount[i:i+10]):
             answer += 1
-        
-        if(day != len(discount)-10):
-            dict_discount[discount[day]] -= 1
-            dict_discount[discount[day+10]] += 1
-        
-        day += 1
     
     return answer
