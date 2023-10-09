42f08eff8fc50595c055672d0250b8e0881dc565
Daehoon Go <50730897+Hoony0321@users.noreply.github.com>
2023-10-09 18:04:52 +0900

    [level 3] Title: 표 편집, Time: 1007.80 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public String solution(int n, int k, String[] cmd) {
+        
+        StringBuilder builder = new StringBuilder();
+        int rowNum = n;
+        Stack<Integer> deleted = new Stack<>();
+        
+        for(String tmp : cmd){
+            if(tmp.length() > 1){
+                int x = Integer.parseInt(tmp.substring(2));
+                if(tmp.charAt(0) == 'U') k -= x;
+                else k += x;
+            } else if (tmp.charAt(0) == 'C'){
+                deleted.push(k);
+                rowNum--;
+                if(rowNum == k) k--;
+            } else {
+                rowNum++;
+                if(deleted.pop() <= k) k++;
+            }
+        }
+        
+        for(int i = 0 ; i < rowNum ; i++)
+            builder.append("O");
+        while(!deleted.empty())
+            builder.insert(deleted.pop(), "X");
+        
+        return builder.toString();
+    }
+}
