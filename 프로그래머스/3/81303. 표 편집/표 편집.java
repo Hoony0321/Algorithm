425d7947e9e3bbb393be24f2b6338749da009d47
Daehoon Go <50730897+Hoony0321@users.noreply.github.com>
2023-10-10 09:29:10 +0900

    [level 3] Title: 표 편집, Time: 357.99 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,31 +1,115 @@
 import java.util.*;
+
+class Node{
+        Node prev = null;
+        Node next = null;
+        int value = -1;
+        
+        Node(int value){
+            this.value = value;
+        }
+    
+        public void setPrev(Node prev){
+            this.prev = prev;
+        }
+    
+        public void setNext(Node next){
+            this.next = next;
+        }
+    
+        public void remove(){
+            Node prevNode = this.prev;
+            Node nextNode = this.next;
+            
+            if(nextNode != null) nextNode.prev = prevNode;
+            if(prevNode != null) prevNode.next = nextNode;
+        }
+    }
+
 class Solution {
+    Node[] nodes;
+    Stack<Node> cash = new Stack<Node>();
+    Node pointer;
+    
+    
+    public void execCmd(String cmd){
+        String[] cmds = cmd.split(" ");
+        if(cmds[0].equals("D")){
+            int num = Integer.parseInt(cmds[1]);
+            while(num > 0){
+                pointer = pointer.next;
+                num--;
+            }
+        }
+        else if(cmds[0].equals("U")){
+            int num = Integer.parseInt(cmds[1]);
+            while(num > 0){
+                pointer = pointer.prev;
+                num--;
+            }
+        }
+        else if(cmds[0].equals("C")){
+            Node nextPointer = pointer.next == null ? pointer.prev : pointer.next;
+            
+            // 원소 삭제 -> 링크 변경
+            pointer.remove();
+            
+            // 캐시 추가
+            cash.push(pointer);
+            
+            // 포인터 변경
+            pointer = nextPointer;
+        }
+        else if(cmds[0].equals("Z")){
+            Node insertNode = cash.pop();
+            
+            // 변경 노드 세팅
+            Node prevNode = insertNode.prev;
+            Node nextNode = insertNode.next;
+            
+            // 링크 변경
+            if(prevNode != null) prevNode.next = insertNode;
+            if(nextNode != null) nextNode.prev = insertNode;
+        }
+    }
+    
     public String solution(int n, int k, String[] cmd) {
+        // 시작 데이터 세팅
+        nodes = new Node[n];
+        for(int i = 0; i < n; i++){
+            nodes[i] = new Node(i);
+        }
         
-        StringBuilder builder = new StringBuilder();
-        int rowNum = n;
-        Stack<Integer> deleted = new Stack<>();
-        
-        for(String tmp : cmd){
-            if(tmp.length() > 1){
-                int x = Integer.parseInt(tmp.substring(2));
-                if(tmp.charAt(0) == 'U') k -= x;
-                else k += x;
-            } else if (tmp.charAt(0) == 'C'){
-                deleted.push(k);
-                rowNum--;
-                if(rowNum == k) k--;
-            } else {
-                rowNum++;
-                if(deleted.pop() <= k) k++;
+        // 노드 연결 진행
+        for(int i = 0; i < n; i++){
+            if(i == 0){
+                nodes[i].setNext(nodes[i+1]);
+            }
+            else if(i == n-1){
+                nodes[i].setPrev(nodes[i-1]);
+            }
+            else{
+                nodes[i].setNext(nodes[i+1]);
+                nodes[i].setPrev(nodes[i-1]);
             }
         }
         
-        for(int i = 0 ; i < rowNum ; i++)
-            builder.append("O");
-        while(!deleted.empty())
-            builder.insert(deleted.pop(), "X");
+        // pointer 시작 위치로 이동
+        pointer = nodes[k];
         
-        return builder.toString();
+        // 명령 실행
+        for(String cmdElem : cmd){
+            execCmd(cmdElem);
+        }
+        
+        String[] answer = new String[n];
+        Arrays.fill(answer,"O");
+        while(!cash.isEmpty()){
+            Node node = cash.pop();
+            answer[node.value] = "X";
+        }
+        
+        return String.join("", answer);
+
     }
 }
