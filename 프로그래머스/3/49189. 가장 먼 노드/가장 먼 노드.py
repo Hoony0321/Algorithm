7b152061510579f06a3586afab1b74a2615e7d70
Daehoon Go <50730897+Hoony0321@users.noreply.github.com>
2024-03-10 16:19:42 +0900

    [level 3] Title: 가장 먼 노드, Time: 31.72 ms, Memory: 22.5 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,33 +1,29 @@
-import heapq
+from collections import deque
 
 def solution(n, edges):
-    graph = [[] for _ in range(n)]
-    distance = [float('inf')] * n
-    visited = [False] * n  # 방문 여부 체크를 위한 배열
-
-    # 그래프를 인접 리스트로 구성
-    for u, v in edges:
-        graph[u-1].append((v-1, 1))  # 가중치가 1인 간선 추가
-        graph[v-1].append((u-1, 1))  # 양방향 그래프를 가정
-
-    def dijkstra(start):
-        queue = [(0, start)]  # (거리, 노드)
-        distance[start] = 0
-
-        while queue:
-            dist, current_node = heapq.heappop(queue)
-            if visited[current_node]:  # 이미 방문한 노드는 스킵
-                continue
-            visited[current_node] = True
-
-            for next_node, weight in graph[current_node]:
-                cost = dist + weight
-                if cost < distance[next_node] and not visited[next_node]:
-                    distance[next_node] = cost
-                    heapq.heappush(queue, (cost, next_node))
-
-    dijkstra(0)  # 0번 노드부터 시작
-
-    # 최대 거리 노드의 수를 계산
-    max_distance = max(distance)
-    return distance.count(max_distance)
+    answer = 0
+    graph = [[] for _ in range(n+1)]
+    
+    for edge in edges:
+        u,v = edge
+        graph[u].append(v)
+        graph[v].append(u)
+    
+    visited = set()
+    visited.add(1)
+    queue = deque()
+    queue.append(1)
+    
+    while queue:
+        answer = 0
+        for _ in range(len(queue)):
+            curNode = queue.popleft()
+            answer += 1
+            
+            for nearNode in graph[curNode]:
+                if nearNode in visited:
+                    continue
+                queue.append(nearNode)
+                visited.add(nearNode)
+    
+    return answer
