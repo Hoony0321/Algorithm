3d0e4d7bd3ba32dd27f27ec51e221d76d28671f5
Daehoon Go <50730897+Hoony0321@users.noreply.github.com>
2025-07-08 15:59:45 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,19 +1,11 @@
 def solution(n, w, num):
-    # 상자의 좌표 계산
-    row = (num - 1) // w
-    pos_in_row = (num - 1) % w
-    if row % 2 == 0:  # 짝수층 → 방향
-        col = pos_in_row
-    else:  # 홀수층 ← 방향
-        col = w - 1 - pos_in_row
-
-    # 위에 있는 층들 중 해당 col에 상자가 있으면 +1
-    count = 1  # 자기 자신
-    for r in range(row + 1, (n + w - 1) // w):
-        if r % 2 == 0:  # → 방향
-            if col < n - r * w:
-                count += 1
-        else:  # ← 방향
-            if w - 1 - col < n - r * w:
-                count += 1
-    return count
+    answer = 0
+    idx = num % w if num % w != 0 else w
+    
+    while(num <= n):
+        answer += 1
+        idx = num % w if num % w != 0 else w
+        num += 2 * (w-idx) + 1
+    
+    
+    return answer
