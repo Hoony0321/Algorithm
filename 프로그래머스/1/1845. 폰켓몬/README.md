d92765a02cb9a4543ac3d746a425e2f73b3da512
Daehun Go <50730897+Hoony0321@users.noreply.github.com>
2025-10-09 17:54:07 +0900

    [level 1] Title: 폰켓몬, Time: 4.26 ms, Memory: 9.63 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=python3) 
 
 ### 성능 요약
 
-메모리: 78.6 MB, 시간: 19.75 ms
+메모리: 9.63 MB, 시간: 4.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 24일 16:23:47
+2025년 10월 09일 17:54:00
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,11 +1,9 @@
 from collections import defaultdict
-
-def solution(nums):    
-    pocketmons = defaultdict(int)
-    
-    for num in nums :
-        pocketmons[num] += 1
-    
-    n = len(nums)
-   
-    return n // 2 if n // 2 <= len(pocketmons.keys()) else len(pocketmons.keys())
+def solution(nums):
+    answer = 0
+    pocketDict = defaultdict(int)
+    for num in nums:
+        pocketDict[num] += 1
+        
+    n = len(nums) // 2
+    return n if len(pocketDict.keys()) >= n else len(pocketDict.keys())
