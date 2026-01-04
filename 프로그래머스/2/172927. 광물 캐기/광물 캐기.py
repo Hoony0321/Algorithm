d9322b275ae45b8b3d5ad840dd3dba11bcbd5ea3
Daehun Go <50730897+Hoony0321@users.noreply.github.com>
2026-01-04 16:22:14 +0900

    [level 2] Title: 광물 캐기, Time: 0.83 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,45 +1,73 @@
 from collections import deque
-from copy import deepcopy
-
-minTotalFatigue = float('INF')
-mineralToNumber = {'diamond' : 0, 'iron' : 1, 'stone' : 2}
-fatigueGraph = [[1,1,1], [5,1,1], [25,5,1]]
-
-def digMinerals(pick, minerals):
-    global minTotalFatigue
-    totalFatigue = 0
-    
-    digCount = 0
-    while minerals and digCount < 5:
-        mineral = minerals.popleft()
-        totalFatigue += fatigueGraph[pick][mineralToNumber[mineral]]
-        digCount += 1
-    
-    return totalFatigue
-
-def dfs(picks, minerals, totalFatigue):
-    global minTotalFatigue
-    if(totalFatigue >= minTotalFatigue): return
-    
-    if( #종료조건
-        (len(minerals) == 0) or
-        (picks[0] == 0 and picks[1] == 0 and picks[2] == 0)
-      ): 
-        minTotalFatigue = min(minTotalFatigue, totalFatigue)
-        return
-    
-    for i in range(0,3):
-        if(picks[i] != 0):
-            newPicks = deepcopy(picks)
-            newMinerals = deepcopy(minerals)
-            newPicks[i] -= 1
-            dfs(newPicks, newMinerals, totalFatigue + digMinerals(i, newMinerals))
-        
-
 def solution(picks, minerals):
-    global minTotalFatigue
-    minTotalFatigue = float('INF')
-    minerals = deque(minerals)
-    dfs(picks, minerals, 0)
+    answer = 0
     
-    return minTotalFatigue
+    # init dict
+    mineral_dict = dict()
+    mineral_dict['diamond'] = 0
+    mineral_dict['iron'] = 1
+    mineral_dict['stone'] = 2
+    
+    fatigue_dict = dict()
+    fatigue_dict['diamond'] = [1,1,1]
+    fatigue_dict['iron'] = [5,1,1]
+    fatigue_dict['stone'] = [25,5,1]
+    
+    # init dp
+    dp = [[[float('inf') for _ in range(6)] for _ in range(6)] for _ in range(6)]
+    dp[picks[0]][picks[1]][picks[2]] = 0
+    
+    # init queue
+    queue = deque()
+    state = [0] + picks
+    queue.append(state)
+    
+    # main process
+    while queue:
+        mineral_idx,diamond,iron,stone = queue.popleft()
+        fatigue = dp[diamond][iron][stone]
+        
+        if mineral_idx >= len(minerals):
+            dp[0][0][0] = min(dp[0][0][0], fatigue)
+            continue
+        
+        count = 0
+        mineral = []
+        while mineral_idx < len(minerals) and count < 5:
+            mineral.append(mineral_dict[minerals[mineral_idx]])
+            mineral_idx += 1
+            count += 1
+            
+        
+        
+        # use diamond
+        if diamond > 0:
+            _fatigue = fatigue
+            for elem in mineral:
+                _fatigue += fatigue_dict['diamond'][elem]
+
+            if _fatigue < dp[diamond-1][iron][stone]:
+                dp[diamond-1][iron][stone] = _fatigue
+                queue.append([mineral_idx,diamond-1,iron,stone])
+        
+        # use iron
+        if iron > 0:
+            _fatigue = fatigue
+            for elem in mineral:
+                _fatigue += fatigue_dict['iron'][elem]
+
+            if _fatigue < dp[diamond][iron-1][stone]:
+                dp[diamond][iron-1][stone] = _fatigue
+                queue.append([mineral_idx,diamond,iron-1,stone])
+                
+        # use stone
+        if stone > 0:
+            _fatigue = fatigue
+            for elem in mineral:
+                _fatigue += fatigue_dict['stone'][elem]
+
+            if _fatigue < dp[diamond][iron][stone-1]:
+                dp[diamond][iron][stone-1] = _fatigue
+                queue.append([mineral_idx,diamond,iron,stone-1])
+        
+    return dp[0][0][0]
